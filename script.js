82e8129ff45a279907ec19253de40022c0cc4619
Edson Rocha <74564387+orochaedsonn@users.noreply.github.com>
2024-11-19 12:59:53 -0300

    lista de tarefas

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#btn-adicionar').click(function(e) {
+    e.preventDefault();
+    var nomeTarefa = $('#nome-tarefa').val();
+    if (nomeTarefa !== '') {
+      $('#lista-tarefas').append('<li>' + nomeTarefa + '</li>');
+      $('#nome-tarefa').val('');
+    }
+  });
+
+  $('#lista-tarefas').on('click', 'li', function() {
+    $(this).toggleClass('concluido');
+  });
+});
